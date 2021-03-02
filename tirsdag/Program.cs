dc7e121c91a86359cd0ec4edde3beb5966f3d29b
tordenilden <fls@tec.dk>
2021-03-02 08:23:33 +0100

    Add project files.

---
+++ tirsdag/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tirsdag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
