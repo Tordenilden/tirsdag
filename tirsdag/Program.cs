052b93da13c5e0839898f55fc098e4bc33f433c8
tordenilden <fls@tec.dk>
2021-03-02 08:38:45 +0100

    test nummer 2

--- tirsdag/Program.cs
+++ tirsdag/Program.cs
@@ -4,10 +4,12 @@ namespace tirsdag
 {
     class Program
     {
+        public string minTredjeÆndring { get; set; }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("første ændring");
+            Console.WriteLine("min anden ændring");
         }
     }
 }
